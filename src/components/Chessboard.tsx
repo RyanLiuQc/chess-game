b0d6feb4d2ff05429d108d68a0115a258da8a483
Ryan Liu <ryanliuqc@gmail.com>
2026-01-06 21:15:53 -0500

    built chessboard element and style

---
+++ src/components/Chessboard.tsx
@@ -0,0 +1,19 @@
+import react from 'react';
+import './Chessboard.css';
+
+const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+const verticalAxis = [1,2,3,4,5,6,7,8];
+
+export default function Chessboard(){
+    let board = [];
+    for (let j=verticalAxis.length-1; j>=0; j--){
+        for ( let i = 0; i<horizontalAxis.length; i++){
+            if ((i+j)%2 == 0){
+                board.push(<div className="black-tile"></div>) 
+            } else {
+                board.push(<div className="white-tile"></div>)
+            }
+        }
+    }
+    return <div id="chessboard">{board}</div>
+}
