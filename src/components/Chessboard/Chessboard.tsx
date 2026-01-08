57b340a69d72330a1ea39830bb84b0cc54f82484
Ryan Liu <ryanliuqc@gmail.com>
2026-01-08 00:00:26 -0500

    added pieces to the board. brute force. create a piece interface.

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -2,21 +2,63 @@ import Tile from "../Tile/Tile"; // imports the function Tile from the Tile.tsx 
 import './Chessboard.css';
 
 const horizontalAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-const verticalAxis = [1,2,3,4,5,6,7,8];
+const verticalAxis = ['1','2','3','4','5','6','7','8'];
+
+interface Piece {
+    // create an instance of this interface with {prop1: value, prop2: value} like an object
+
+    image: string;
+    x: number;
+    y: number 
+}
+
+const pieces: Piece[] = []; // similar to java, declaring the type of the elements in pieces\
+
+for (let i = 0; i<8; i++) {
+    pieces.push({image: 'assets/images/pawn_b.png', x:i,y:6})
+    pieces.push({image: 'assets/images/pawn_w.png', x:i,y:1})
+}
+pieces.push({image: 'assets/images/rook_b.png', x:0,y:7})
+pieces.push({image: 'assets/images/rook_b.png', x:7,y:7})
+
+pieces.push({image: 'assets/images/rook_w.png', x:0,y:0})
+pieces.push({image: 'assets/images/rook_w.png', x:7,y:0})
+
+pieces.push({image: 'assets/images/knight_b.png', x:1,y:7})
+pieces.push({image: 'assets/images/knight_b.png', x:6,y:7})
+
+pieces.push({image: 'assets/images/knight_w.png', x:1,y:0})
+pieces.push({image: 'assets/images/knight_w.png', x:6,y:0})
+
+pieces.push({image: 'assets/images/bishop_b.png', x:2,y:7})
+pieces.push({image: 'assets/images/bishop_b.png', x:5,y:7})
+
+pieces.push({image: 'assets/images/bishop_w.png', x:2,y:0})
+pieces.push({image: 'assets/images/bishop_w.png', x:5,y:0})
+
+pieces.push({image: 'assets/images/king_b.png', x:4,y:7})
+pieces.push({image: 'assets/images/queen_b.png', x:3,y:7})
+
+pieces.push({image: 'assets/images/king_w.png', x:4,y:0})
+pieces.push({image: 'assets/images/queen_w.png', x:3,y:0})
+
 
 export default function Chessboard(){
     let board = [];
-    for (let j=verticalAxis.length-1; j>=0; j--){
-        for ( let i = 0; i<horizontalAxis.length; i++){
+    for (let j=verticalAxis.length-1; j>=0; j--){ // render from h to a (j=7 to 0)
+        for ( let i = 0; i<horizontalAxis.length; i++){ // render from 1 to 8 in standard chessboard axis (i=0 to 7)
             const number = j+i;
+            let img_path = undefined; // undefined for declared but unassigned variable
 
-            board.push(<Tile number={number}/>)
+            pieces.forEach(p => {
+                if (p.x === i && p.y === j){
+                    img_path = p.image
+                }
+            })
 
-            // if ((i+j)%2 == 0){
-            //     board.push(<Tile/>) 
-            // } else {
-            //     board.push(<Tile/>)
-            // }
+            board.push(<Tile image={img_path} number={number}/>) //the props image and number are create in Tile.tsx file and the way they are used are also there
+
+          
         }
     }
     return <div id="chessboard">{board}</div>
--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,16 +1,26 @@
 import './Tile.css'
 
-interface Props { // Props for property, you can use this
-    number: number
+interface Props { // Props for property, you can use this when creating an element.
+    number: number;
+    // let image to be of type string OR (union) undefined since we don't want pieces on every single tile
+    image: string | undefined;
 }
 
-export default function Tile( { number } : Props) {
+export default function Tile( { number, image } : Props) { // the parameters are props
     if (number % 2 === 0){
         // the current folder of the relative path that the src looks at is the public folder
         // each image is inside a div with class tile and the image has the class img
-        return <div className='tile black-tile'><img src="assets/images/pawn_b.png" /></div> 
+        return (
+            <div className='tile black-tile'>
+                <img src={image} />
+            </div> 
+        )
     } else {
-        return <div className='tile white-tile'></div>
+        return (
+            <div className='tile white-tile'>
+                <img src={image} />
+            </div>
+        )
     }
 
     
