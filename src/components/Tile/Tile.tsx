fa514b3125dbe72a8885b1f55a709e9f925ab7bf
Ryan Liu <ryanliuqc@gmail.com>
2026-01-07 23:07:55 -0500

    moved image to public folder and rendered first pawn image

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -6,7 +6,9 @@ interface Props { // Props for property, you can use this
 
 export default function Tile( { number } : Props) {
     if (number % 2 === 0){
-        return <div className='tile black-tile'></div>
+        // the current folder of the relative path that the src looks at is the public folder
+        // each image is inside a div with class tile and the image has the class img
+        return <div className='tile black-tile'><img src="assets/images/pawn_b.png" /></div> 
     } else {
         return <div className='tile white-tile'></div>
     }
